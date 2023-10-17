88936fce4f667cd62a9b356b31281d1951dc1f90
Alisson Rego <alissondroid07@gmail.com>
2023-10-17 08:08:23 -0300

    Commit inicial.

---
+++ spring-jwt/src/test/java/edu/alisson/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alisson.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
